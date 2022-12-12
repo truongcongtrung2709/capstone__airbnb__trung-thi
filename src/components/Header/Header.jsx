7f251909555d29036bb8a08992a6c4362d122589
Trung <truongcongtrung2709@gmail.com>
2022-12-12 23:28:01 +0700

    creating header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
 import "./header.scss";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="header__container">
+        <Navbar variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">
+              <img
+                alt=""
+                src=""
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+              />{" "}
+              airbnb
+            </Navbar.Brand>
+          </Container>
+        </Navbar>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
